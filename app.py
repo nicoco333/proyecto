ad3806785fff924411a3f337cea04413e49002e5
Nicolas <94737@sistemas.frc.utn.edu.ar>
2025-12-21 20:01:28 -0300

    Correccion: Crear tablas al iniciar app para Render

--- app.py
+++ app.py
@@ -56,9 +56,7 @@ def delete(id):
     # 3. Volvemos a la lista
     return redirect(url_for('home'))
 
-if __name__ == '__main__':
-    # Este comando crea las tablas si no existen
-    with app.app_context():
+with app.app_context():
         db.create_all()
-    
+if __name__ == '__main__':
     app.run(debug=True)
